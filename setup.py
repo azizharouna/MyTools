0dd96c663a1d8c1ef395a82853273c3c14261889
Aziz <abdoulenergy@gmail.com>
2023-10-09 13:30:35 +0200

    corrected

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytools',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn'
+    ],
+    author='Abdoul Aziz Moussa',
+    author_email='abdoulenergy@gmail.com',
+    description='A Python Mother Class for Data Analysis',
+    license='MIT',
+)
